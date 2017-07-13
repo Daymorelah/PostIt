cd2b3425c0caf6dcc07995db2eaa89499e2d30ad
Daymorelah <demola.hussainin@gmail.com>
2017-07-13 22:00:51 +0100

    Install package to allow connection to Heroku

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,4 @@
-/*
-const users = require('./users');
-const groups = require('./groups');
-const messages = require('./message');
-*/
+
 import Users from './users';
 import Groups from './groups';
 
