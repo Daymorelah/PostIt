1290a73c8687e9b1c34610b7a76c13a6b677b19d
Daymorelah <demola.hussainin@gmail.com>
2017-07-18 08:12:11 +0100

    remove UI files

--- server/config/config.js
+++ server/config/config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  "development": {
-    "username": "postgres",
-    "password": "andelabootcamp24",
-    "database": "test2",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  development: {
+    username: 'postgres',
+    password: 'andelabootcamp24',
+    database: 'test2',
+    host: '127.0.0.1',
+    dialect: 'postgres'
   },
-  "test": {
-    "username": "postgres",
-    "password": "andelabootcamp24",
-    "database": "test2",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    username: 'postgres',
+    password: 'andelabootcamp24',
+    database: 'test2',
+    host: '127.0.0.1',
+    dialect: 'postgres'
   },
-  "production": {
-    "use_env_variable": "DATABASE_URL"
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
   }
 }
