72ffadc372b2f22d73d09106fbdc8bcea9916bfb
Daymorelah <demola.hussainin@gmail.com>
2017-10-05 08:57:26 +0100

    Set production configurations in config file

--- server/config/config.js
+++ server/config/config.js
@@ -19,10 +19,14 @@
    },
 
    'production': {
-     'username': 'root',
-     'password': null,
-     'database': 'database_production',
-     'host': '127.0.0.1',
-     'dialect': 'mysql'
+     'username': process.env.USER,
+     'password': process.env.PASSWORD,
+     'database': process.env.DATABASE,
+     'host'    : process.env.HOST,
+     'dialect' : 'postgres',
+     'ssl'     : true,
+     'dialectOptions': {
+       ssl: true
+     }
    }
  };
