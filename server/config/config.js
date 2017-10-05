fcf7a2da1b7c7ef8325849960e30997105e019d7
Daymorelah <demola.hussainin@gmail.com>
2017-10-05 10:45:35 +0100

    Exclude dist folder in code climate

--- server/config/config.js
+++ server/config/config.js
@@ -24,6 +24,7 @@
      'database': process.env.DATABASE,
      'host'    : process.env.HOST,
      'dialect' : 'postgres',
+     'logging' : false,
      'ssl'     : true,
      'dialectOptions': {
        ssl: true
