a572ba446ff12d70204126515669e326b2a7c116
Daymorelah <demola.hussainin@gmail.com>
2017-11-24 11:21:22 +0100

    Fix swagger API documentation

--- server/app.js
+++ server/app.js
@@ -9,16 +9,17 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve('apiDocDist')));
 
 if( app.get('env') !== 'test'){/* istanbul ignore next */
   app.use(logger('dev')); 
 }
 
-routes(app);
-
 app.get('/api/v1/documentation', (req, res)=>{
   res.sendFile('index.html', {root:path.resolve('apiDocDist')});
 });//end of get method.
+
+routes(app);
 
 const port = process.env.PORT || 1111;
 
