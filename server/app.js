0b309e323192cc35f6166085c3ed3a63cc9bb7a5
Daymorelah <demola.hussainin@gmail.com>
2017-12-07 23:07:30 +0100

    Include route for serving the front-end

--- server/app.js
+++ server/app.js
@@ -9,21 +9,31 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve('client','public')));
 app.use(express.static(path.resolve('apiDocDist')));
 
 // app.use( (req, res, next) =>{
-  // '*' is not good for production. Only if the API consumable is for public use.
-  // res.header('Access-Control-Allow-Origin', '*'); //allow another domain use ur api.
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  // next();});
+//   //'*' is not good for production. Only if the API consumable is for public use.
+//   res.header('Access-Control-Allow-Origin', '*'); //allow another domain use ur api.
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();});
 
 if( app.get('env') !== 'test'){/* istanbul ignore next */
   app.use(logger('dev')); 
 }
 
+
+
+//serve API documentation when this route is used.
 app.get('/api/v1/documentation', (req, res)=>{
   res.sendFile('index.html', {root:path.resolve('apiDocDist')});
-});//end of get method.
+});//end of get http method.
+
+//Serve front-end
+app.get('/', (req, res)=>{
+  res.sendFile('index.html', {root:path.resolve('client','public')});
+});//end of get http method
+//import routes here.
 
 routes(app);
 
